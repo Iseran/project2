9a15f9395a35c7a8d726d1b918e9963867fa4885
Iseran <121060060+Iseran@users.noreply.github.com>
2022-12-19 15:55:19 -0500

    Add files via upload

---
+++ Project2/test.js
@@ -0,0 +1,27 @@
+function myFunction1() {
+  document.getElementById("desc1").innerHTML = "Bypass makes your sound clean";
+}
+function myFunction2() {
+  document.getElementById("desc2").innerHTML = "Allows for playback and looping of sound";
+}
+function myFunction3() {
+  document.getElementById("desc3").innerHTML = "Dirty reverb, bitcrush, and pitching effects";
+}
+function myFunction4() {
+  document.getElementById("desc4").innerHTML = "Tells you the pitch and note you are playing";
+}
+function myFunction5() {
+  document.getElementById("desc5").innerHTML = "Plays what you are playing but slightly out of sync";
+}
+function myFunction6() {
+  document.getElementById("desc6").innerHTML = "Adjustable gain with scoop and crunch effects";
+}
+function myFunction7() {
+  document.getElementById("desc7").innerHTML = "A fuzzy distortion effect";
+}
+function myFunction8() {
+  document.getElementById("desc8").innerHTML = "The iconic distortion pedal with adjustable distance";
+}
+function myFunction9() {
+  document.getElementById("desc9").innerHTML = "Plays notes on top of yours that is an ocatave higher";
+}
